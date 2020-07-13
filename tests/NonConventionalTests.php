977d071d9c1b2401ba91b11bd2a744568f105054
Jorge <jomisacu.software@gmail.com>
2020-07-13 19:51:55 -0400

    first commit

---
+++ tests/NonConventionalTests.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author Jorge Sanchez <jomisacu.software@gmail.com>
+ */
+
+
+final class NonConventionalTests
+{
+    public static function test1()
+    {
+        echo 'the script fails';
+        exit(1);
+    }
+}
